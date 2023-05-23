ef0ee3deeccb2d21e21ecd8dcd4401e627ea54df
Jide Muritala <115728688+Jide-Muritala@users.noreply.github.com>
2023-05-23 13:37:10 +0000

    refactor-revert

--- src/app.js
+++ src/app.js
@@ -8,10 +8,6 @@ window.onload = () => {
   });
 };
 
-function getRandomElement(array) {
-  return array[Math.floor(Math.random() * array.length)];
-}
-
 let generateExcuse = () => {
   const pronoun = ["A", "The"];
   const subject = ["jogger", "racoon", "dog", "driver", "clown", "pine cone"];
